742c7dfa84e216812dbcecd1c398e46c0d3fae0a
Eduardo <eduardokreve283@gmail.com>
2019-04-22 16:03:12 -0300

    Adicionar TodoList básica

--- todoApp/src/TodoApp.js
+++ todoApp/src/TodoApp.js
@@ -4,10 +4,10 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
 import rootReducer from './reducers';
-
 import devToolsEnhancer from 'remote-redux-devtools';
-
 
 const store = createStore(rootReducer, devToolsEnhancer()); 
 
@@ -17,6 +17,8 @@ export default class TodoApp extends React.Component {
             <Provider store={store}>
                 <View style={styles.container}>
                     <TodoForm />
+
+                    <TodoList/>
                 </View>
             </Provider>
         );
---
+++ todoApp/src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {connect} from 'react-redux';
+
+const TodoList = ({ todos = [] }) => (
+    <View>
+        {todos.map(todo => <Text key={todo.id}>{todo.text}</Text>)}
+    </View>
+);
+
+const styles = StyleSheet.create({
+
+})
+
+const mapStateToProps = state => {
+    const { todos } = state;
+    return { todos };
+}
+
+
+export default connect(mapStateToProps)(TodoList);
