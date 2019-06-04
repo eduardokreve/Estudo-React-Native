46ae1f5a4e98630921d1d6b77ffd9689b860339b
Eduardo <eduardokreve283@gmail.com>
2019-06-04 16:48:58 -0300

    Prototipo telas

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -9,7 +9,9 @@ class Person extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text onPress={()=> this.setState({name : "Dellen"})}>My name is {this.state.name}</Text>	
+				<Text onPress={()=> this.setState(
+					{name : "Eduardo"}
+				)}>My name is {this.state.name}</Text>	
 			</View>
 		);
   	}
@@ -26,13 +28,6 @@ const styles = StyleSheet.create({
 	container: {
 	 flex: 1,
 	 justifyContent: 'center',
-	},
-	buttonContainer: {
-	  margin: 20
-	},
-	alternativeLayoutButtonContainer: {
-	  margin: 20,
-	  flexDirection: 'row',
-	  justifyContent: 'space-between'
+	 alignItems: 'center'
 	}
   });
