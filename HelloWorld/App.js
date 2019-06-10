c548476bdb28deb10d5b67b4143a51f9c9a639bf
kreve <eduardokreve283@gmail.com>
2019-06-10 11:59:54 -0300

    Continuar no estagio

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
-import { View, StyleSheet, Image, AppRegistry} from 'react-native';
-import { useScreens } from 'react-native-screens';
+import { View, Image} from 'react-native';
 import LoginPage from './src/pages/LoginPage';
-
-useScreens(); //otimização do react-navigation
 
 export default class App extends Component {
 	render() {
