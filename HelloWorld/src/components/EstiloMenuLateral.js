ccd08d96c1777624e90f1b4760531892ebf4846b
Eduardo <eduardokreve283@gmail.com>
2019-06-24 17:02:15 -0300

    Estilizando os itens do Drawer Navigator

--- HelloWorld/src/components/EstiloMenuLateral.js
+++ HelloWorld/src/components/EstiloMenuLateral.js
@@ -22,22 +22,69 @@ export default class EstiloMenuLateral extends Component {
             <View style = {styles.container}>
                 
                 <View style = {styles.header}> 
-                        <TouchableOpacity onPress={this.navigateToScreen("Calendario")}>
+                        <TouchableOpacity/*onPress={this.navigateToScreen("Tela")}*/>
                             <Text style={styles.nomeUser}>
                                 Usuário
                             </Text> 
-                            <Image source={require('../../assets/icone_usuario.png')}/>
                         </TouchableOpacity>
+                        <Image style={styles.perfil} source={require('../../assets/icone_usuario.png')}/>       
                 </View>
-
+              
                 <View style={styles.scrowView}>
+                    <View style={{flex: 1, flexDirection: 'column', marginTop:'5%', justifyContent: 'space-around'}}>
+                       
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Calendario")}>
+                            <Image style={styles.image} source={require('../../assets/calendario_academico.png')}/> 
+                            <Text style={styles.menus}>
+                                Calendário Acadêmico
+                            </Text> 
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Cardapio")}>
+                            <Image style={styles.image} source={require('../../assets/ru.png')}/> 
+                            <Text style={styles.menus}>
+                                Cardápio R.U
+                            </Text> 
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Uteis")}>
+                            <Image style={styles.image} source={require('../../assets/links.png')}/> 
+                            <Text style={styles.menus}>
+                                Úteis
+                            </Text> 
+                        </TouchableOpacity>
 
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Eventos")}>
+                            <Image style={styles.image} source={require('../../assets/eventos.png')}/> 
+                            <Text style={styles.menus}>
+                                Eventos
+                            </Text> 
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Aula")}>
+                            <Image style={styles.image} source={require('../../assets/horario_aula.png')}/> 
+                            <Text style={styles.menus}>
+                                Horários aula
+                            </Text> 
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity style={styles.touch} onPress={this.navigateToScreen("Onibus")}>
+                            <Image style={styles.image} source={require('../../assets/horario_onibus.png')}/> 
+                            <Text style={styles.menus}>
+                                Horário de ônibus
+                            </Text> 
+                        </TouchableOpacity>
+                    </View>
                 </View>
 
                 <View style={styles.footerView}>
-
+                    <TouchableOpacity style={{padding:'5%'}} onPress={this.navigateToScreen("")}>
+                        <Image style={{justifyContent:'flex-start', alignItems:'center', bottom:'5%'}} source={require('../../assets/configuração.png')}/> 
+                            <Text style={styles.menConf}>
+                                Configurações
+                            </Text> 
+                    </TouchableOpacity>
                 </View>
-
             </View>
         );
     }
@@ -49,36 +96,64 @@ const styles = StyleSheet.create({
         height:'100%'
     },
     header: {
-        elevation:2,
-        backgroundColor:'white',
+        elevation:1.5,
+        backgroundColor:'#f7f7f7',
         width:'100%',
-        height:'20%',
+        height:'15%',
         borderColor:'#EBEBEB',
         borderWidth: 1,
-        flex:1
+        flex:1,
+    },
+    perfil:{
+        left:'3%',
+        bottom:'33%'
     },
     nomeUser: {
         fontSize:28,
         color: '#7a7a7a',
-        left:'50%',
-        top:'50%',
-        justifyContent: 'center',
-        alignItems: 'center',
+        left:'45%',
+        top:'88%',
+        fontWeight:'bold',
     },
     scrowView: {
         elevation:1,
         width:'100%',
-        height:'70%',
+        height:'75%',
+        borderWidth: 1,
+        borderColor:'#EBEBEB',
+        backgroundColor:'#f7f7f7',
+    },
+    touch:{
+        alignContent:'flex-start',
+        alignItems:'flex-start', 
+        padding:'2%'
+    },  
+    imagem:{
+        flex: 1,
+        resizeMode: 'contain', 
+    },
+    menus:{
+        alignItems:'center',
+        justifyContent:'center',
+        color: '#7a7a7a',
+        fontSize: 18,
+        left:'22%',
+        bottom:'50%'
+    },
+    footerView: {
+        width:'100%',
+        height:'10%',
+        elevation:1.5,
+        backgroundColor:'#f7f7f7',
         borderWidth: 1,
         borderColor:'#EBEBEB'
     },
-    footerView: {
-        elevation:2,
-        backgroundColor:'white',
-        width:'100%',
-        height:'10%',
-        borderWidth: 1,
-        borderColor:'#EBEBEB'
-    }
-
+    menConf:{
+        alignItems:'center',
+        justifyContent:'center',
+        color: '#7a7a7a',
+        fontSize:20,
+        left:'30%',
+        bottom:'43%'
+    },
 })
